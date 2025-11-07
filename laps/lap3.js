217d8a495dbbd93424e4612b0f5da8cc0d295e63
Nguyen Trung <nguyentrung9112006-tech@gmail.com>
2025-11-07 15:31:36 +0700

    cập nhật

---
+++ laps/lap3.js
@@ -0,0 +1,19 @@
+// Function 1
+function multiply(a, b) {
+    return a * b;
+}
+
+// Function 2
+function isPositive(number) {
+    return number >= 0;
+}
+
+// Function 3
+function getRandomNumber() {
+    return Math.random();
+}
+
+// Function 4
+document.addEventListener("click", function () {
+    console.log("Clicked!");
+});
