4c08f5b8e95a4957fb71132b05ac0b459a0ec0b6
Nguyen Trung <nguyentrung9112006-tech@gmail.com>
2025-11-14 15:40:58 +0700

    add commit

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,58 @@
+function App() {
+  // mock data
+  const tours = [
+    {
+      id: 1,
+      title: 'Tour Du Lịch Cuba',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
+    {
+      id: 2,
+      title: 'Tour Du Lịch Hoa Ky',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
+    },
+  ]
+  return (
+    <div>
+      <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
+        Tour Nội Địa
+      </h1>
+      <p className="text-xl font-medium my-2 px-2">
+        Các chuyến đi đồng hành cùng chúng tôi là khoảnh khắc đặc biêt, luôn sẵn
+        sàng tạo ra những trải nghiệm độc đáo và không quên cho du khách, giúp
+        mang đến những chuyến hành trình tuyệt vời.
+      </p>
+      <div className="flex gap-2">
+        {tours.map(tour => (
+          <div key={tour.id}>
+            <img src={tour.image} width={500} alt="" height={300} />
+            <h2 className="text-3xl font-bold text-orange-300">{tour.title}</h2>
+          </div>
+        ))}
+
+        {/* <div>
+          <img src={tours[0].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[0].title}
+          </h2>
+        </div>
+        <div>
+          <img src={tours[1].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[1].title}
+          </h2>
+        </div>
+        <div>
+          <img src={tours[2].image} width={500} alt="" height={300} />
+          <h2 className="text-3xl font-bold text-orange-300">
+            {tours[2].title}
+          </h2>
+        </div> */}
+      </div>
+    </div>
+  )
+}
+
+export default App
