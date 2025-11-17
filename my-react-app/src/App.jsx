25c3be4a2fa04571aa4ac1fd2bd37e1f89fa11f5
Nguyen Trung <nguyentrung9112006-tech@gmail.com>
2025-11-17 15:49:22 +0700

    êmc

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,4 +1,6 @@
-import Header from "./coponents/Header"
+import Footer from './coponents/Footer'
+import Header from './coponents/Header'
+
 function App() {
   // mock data
   const tours = [
@@ -14,45 +16,44 @@ function App() {
       image:
         'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
     },
+    {
+      id: 3,
+      title: 'Tour Du Lịch Nhat Ban Update',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
+    },
+    {
+      id: 4,
+      title: 'Tour Du Lịch Nhat Ban Update',
+      image:
+        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
+    },
   ]
   return (
-    <div>
-      <Header></Header>
-      <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
-        Tour Nội Địa
-      </h1>
-      <p className="text-xl font-medium my-2 px-2">
-        Các chuyến đi đồng hành cùng chúng tôi là khoảnh khắc đặc biêt, luôn sẵn
-        sàng tạo ra những trải nghiệm độc đáo và không quên cho du khách, giúp
-        mang đến những chuyến hành trình tuyệt vời.
-      </p>
-      <div className="flex gap-2">
-        {tours.map(tour => (
-          <div key={tour.id}>
-            <img src={tour.image} width={500} alt="" height={300} />
-            <h2 className="text-3xl font-bold text-orange-300">{tour.title}</h2>
-          </div>
-        ))}
+    <div className="min-h-screen flex flex-col justify-between">
+      <Header />
+      <main>
+        <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
+          Tour Nội Địa
+        </h1>
+        <p className="text-xl font-medium my-2 px-2">
+          Các chuyến đi đồng hành cùng chúng tôi là khoảnh khắc đặc biêt, luôn
+          sẵn sàng tạo ra những trải nghiệm độc đáo và không quên cho du khách,
+          giúp mang đến những chuyến hành trình tuyệt vời.
+        </p>
+        <div className="flex gap-2">
+          {tours.map(tour => (
+            <div key={tour.id}>
+              <img src={tour.image} width={500} alt="" height={300} />
+              <h2 className="text-3xl font-bold text-orange-300">
+                {tour.title}
+              </h2>
+            </div>
+          ))}
+        </div>
+      </main>
 
-        {/* <div>
-          <img src={tours[0].image} width={500} alt="" height={300} />
-          <h2 className="text-3xl font-bold text-orange-300">
-            {tours[0].title}
-          </h2>
-        </div>
-        <div>
-          <img src={tours[1].image} width={500} alt="" height={300} />
-          <h2 className="text-3xl font-bold text-orange-300">
-            {tours[1].title}
-          </h2>
-        </div>
-        <div>
-          <img src={tours[2].image} width={500} alt="" height={300} />
-          <h2 className="text-3xl font-bold text-orange-300">
-            {tours[2].title}
-          </h2>
-        </div> */}
-      </div>
+      <Footer />
     </div>
   )
 }
